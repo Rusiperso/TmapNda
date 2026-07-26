fef3362654343f4671b48c1063be5eceb9386a22
Rusiperso <jaeeok@rusiperso.dev>
2026-07-26 23:12:03 +0000

    chore: bump version to 1.0.47

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.tmap.nda"
         minSdk = 24
         targetSdk = 36
-        versionCode = 46
-        versionName = "1.0.46"
+        versionCode = 47
+        versionName = "1.0.47"
     }
 
     signingConfigs {
