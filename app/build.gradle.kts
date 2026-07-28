574d4c6d15f4d7d84696bd1c74da65fe5908e838
Rusiperso <jaeeok@rusiperso.dev>
2026-07-28 03:14:30 +0000

    bump 1.0.64

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.tmap.nda"
         minSdk = 26
         targetSdk = 36
-        versionCode = 63
-        versionName = "1.0.63"
+        versionCode = 64
+        versionName = "1.0.64"
     }
 
     signingConfigs {
