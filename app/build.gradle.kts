75cc70e3aedecd0e780e01133edf6279a87c98cb
Rusiperso <jaeeok@rusiperso.dev>
2026-07-28 21:23:37 +0000

    v1.0.74

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.tmap.nda"
         minSdk = 26
         targetSdk = 36
-        versionCode = 73
-        versionName = "1.0.73"
+        versionCode = 74
+        versionName = "1.0.74"
     }
 
     signingConfigs {
