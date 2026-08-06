f6dbdbde7466e329d56884723a05b2e1a85d14f9
Rusiperso <jaeeok.cho@icloud.com>
2026-08-06 10:35:21 +0000

    v4.17 빌드

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.tmap.nda"
         minSdk = 26
         targetSdk = 36
-        versionCode = 148
-        versionName = "4.16"
+        versionCode = 149
+        versionName = "4.17"
     }
 
     signingConfigs {
