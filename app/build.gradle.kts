21d030fb608dd678d397d995c689ec8de62f603a
Rusiperso <jaeeok.cho@icloud.com>
2026-08-13 11:37:50 +0000

    버전 8.6 (versionCode 194)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.tmap.nda"
         minSdk = 26
         targetSdk = 36
-        versionCode = 193
-        versionName = "8.5"
+        versionCode = 194
+        versionName = "8.6"
     }
 
     signingConfigs {
