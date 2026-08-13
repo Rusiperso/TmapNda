be984e7d7c82d8cf82950d8073816d39a51a85dd
Rusiperso <jaeeok.cho@icloud.com>
2026-08-13 10:27:12 +0000

    버전 8.5 - 위성지도 지원 여부 조사용 로그 빌드

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.tmap.nda"
         minSdk = 26
         targetSdk = 36
-        versionCode = 192
-        versionName = "8.4"
+        versionCode = 193
+        versionName = "8.5"
     }
 
     signingConfigs {
