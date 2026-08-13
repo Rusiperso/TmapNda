beff3f0978e9fb16ac70e3778c285a56fe88c0e8
재억 <rusiperso@users.noreply.github.com>
2026-08-13 00:12:23 +0000

    버전 8.2 (versionCode 190)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.tmap.nda"
         minSdk = 26
         targetSdk = 36
-        versionCode = 189
-        versionName = "8.1"
+        versionCode = 190
+        versionName = "8.2"
     }
 
     signingConfigs {
