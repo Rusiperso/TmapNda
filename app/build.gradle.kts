7da76dad67444fbd26ec807691352e336f785950
Rusiperso <rusiperso@users.noreply.github.com>
2026-08-14 11:46:51 +0000

    버전 9.4로 올림

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.tmap.nda"
         minSdk = 26
         targetSdk = 36
-        versionCode = 201
-        versionName = "9.3"
+        versionCode = 202
+        versionName = "9.4"
     }
 
     signingConfigs {
