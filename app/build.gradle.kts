1bf455f69948d388d0c3002e5637742ea9bae6ef
Rusiperso <jaeeok.cho@icloud.com>
2026-08-17 14:42:49 +0000

    chore: 버전 10.8 (versionCode 216)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.tmap.nda"
         minSdk = 26
         targetSdk = 36
-        versionCode = 215
-        versionName = "10.7"
+        versionCode = 216
+        versionName = "10.8"
     }
 
     signingConfigs {
