9339c1b224e25037056be9a88f5ea3125b28bac4
Rusiperso <jaeeok.cho@icloud.com>
2026-08-29 07:46:25 +0900

    미니 플레이어 버튼이 화면 클리핑으로 안 보이던 문제 수정 (v19.2.44)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.tmap.nda"
         minSdk = 26
         targetSdk = 36
-        versionCode = 358
-        versionName = "19.2.43"
+        versionCode = 359
+        versionName = "19.2.44"
     }
 
     signingConfigs {
